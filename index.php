0c7f65b691ff89a8c29bf574a91504a8fa56dbcd
Tristan Bennett <tmbennett2011@msn.com>
2018-02-20 09:41:32 -0700

    snap api calls

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+
+		<title>Snap Bacon Challenge</title>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script type="text/javascript" src="bacon.js"></script>
+
+	</head>
+	<body>
+		<h1>Snap Bacon Challenge</h1>
+		<div id="baconIpsumOutput"></div>
+	</body>
+</html>
